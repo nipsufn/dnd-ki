b66a8cf062e923fadd749ef9d0ab495917083c90
nipsufn <ferdynand.naczynski@gmail.com>
2020-05-05 23:14:21 +0200

    improve scripts

--- tag.py
+++ tag.py
@@ -2,12 +2,12 @@
 
 import os
 import sys
+
+import argparse
+import logging
 import re
 from html.parser import HTMLParser
-
-
-
-fileList = [f for f in os.listdir() if os.path.isfile(f)]
+import datetime
 
 class tagParser(HTMLParser):
   def __init__(self):
@@ -39,6 +39,7 @@ class tagCreator(HTMLParser):
     self.curentHtmlTag = []
     self.tags = tags
     self.text = ""
+    self.count = 0
     super().__init__()
   
   def handle_starttag(self, tag, attrs):
@@ -54,6 +55,7 @@ class tagCreator(HTMLParser):
 
   def handle_data(self, data):
     if len(self.curentHtmlTag) == 0 or self.curentHtmlTag[0] != "a":
+      self.count += 1
       for pair in self.tags:
         #all user-tags `[whatever](Actual Name)`
         regex = r"(\[[ \w].+?\]\()" + r"("+pair[1]+r")" + r"(\))"
@@ -65,39 +67,78 @@ class tagCreator(HTMLParser):
         substitute = r"\1[\2](#"+pair[0]+r")\3"
         data = re.sub(regex, substitute, data)
     self.text += data
-    
-whitelist = [
-  sys.argv[0][2:],
-  ".gitignore",
-  "requirements.txt",
-  ".travis.yml",
-  "test.py",
-  "tag.py"
-  ]
-  
-tagRetreiver = tagParser()
-# pass 1 - generate tags
-for filePath in fileList:
-  if filePath in whitelist:
-    continue
-  with open(filePath, 'r', encoding='utf-8') as fileStream:
-    tagRetreiver.feed(fileStream.read())
 
-# pass 2 - use tags to create links
-for filePath in fileList:
-  if filePath in whitelist:
-    continue
-  text = None
-  print (filePath)
-  with open(filePath, 'r', encoding='utf-8') as fileStream:
-    text = fileStream.read()
-    tagger = tagCreator(tagRetreiver.tags)
-    tagger.feed(text)
-    tagger.close()
-    text = tagger.text
-    del tagger
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", "--debug", "-v", "--verbose", action="store_true",
+                      help="debug mode")
+  parser.add_argument("-i", "--ignore", nargs='+', type=str,
+                      help="space separated list of files to be ignored",
+                      default=list())
+  args = parser.parse_args()
 
-  with open(filePath, 'w', encoding='utf-8') as fileStream:
-    fileStream.write(text)
-    
+  logger = logging.getLogger('journal_tagger')
+  log_handler = logging.StreamHandler()
+  log_handler.setFormatter(
+      logging.Formatter(
+          '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+          )
+      )
+  logger.addHandler(log_handler)
+  if args.debug:
+      logger.setLevel(logging.DEBUG)
+  else:
+      logger.setLevel(logging.INFO)
+
+  whitelist = [
+    ".gitignore",
+    "requirements.txt",
+    ".travis.yml",
+    "test.py",
+    "tag.py"
+    ]
+  whitelist.extend(args.ignore)
+  with open(".gitignore") as gitignore:
+    whitelist.extend(gitignore.read().splitlines())
+  logger.debug("main: Ignored files: %s", str(whitelist))
+  files = [f for f in os.listdir() if os.path.isfile(f)]
+  files = list(set(files) - set(whitelist))
+  logger.debug("main: List of files: %s", str(files))
+
+  tagRetreiver = tagParser()
+  # pass 1 - generate tags
+  for filePath in files:
+    with open(filePath, 'r', encoding='utf-8') as fileStream:
+      tagRetreiver.feed(fileStream.read())
+
+  logger.debug("Tag count: " + str(len(tagRetreiver.tags)))
+  # pass 2 - use tags to create links
+  for filePath in files:
+    text = None
+    logger.info(filePath)
+    with open(filePath, 'r', encoding='utf-8') as fileStream:
+      start = datetime.datetime.now()
+      text = fileStream.read()
+      elapsed_time = datetime.datetime.now() - start
+      logger.debug("\tread:  " + str(elapsed_time.seconds) + ":" + str(elapsed_time.microseconds) + "sec")
+
+      start = datetime.datetime.now()
+      tagger = tagCreator(tagRetreiver.tags)
+      elapsed_time = datetime.datetime.now() - start
+      logger.debug("\tinit:  " + str(elapsed_time.seconds) + ":" + str(elapsed_time.microseconds) + "sec")
+
+      start = datetime.datetime.now()
+      tagger.feed(text)
+      elapsed_time = datetime.datetime.now() - start
+      logger.debug("\tfeed:  " + str(elapsed_time.seconds) + ":" + str(elapsed_time.microseconds) + "sec")
+      logger.debug("\tcount: " + str(tagger.count))
+      tagger.close()
+      text = tagger.text
+      del tagger
+
+    with open(filePath, 'w', encoding='utf-8') as fileStream:
+      fileStream.write(text)
+
+if __name__ == "__main__":
+  main()
  
--- test.py
+++ test.py
@@ -2,21 +2,11 @@
 
 import os
 import sys
+
+import argparse
+import logging
 import re
 import requests
-
-fileList = [f for f in os.listdir() if os.path.isfile(f)]
-tags = []
-refs = []
-
-whitelist = [
-  sys.argv[0][2:],
-  ".gitignore",
-  "requirements.txt",
-  ".travis.yml",
-  "test.py",
-  "tag.py"
-  ]
 
 feedback = ""
 
@@ -24,91 +14,131 @@ def write(text):
   global feedback
   feedback += text + "\n"
 
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", "--debug", "-v", "--verbose", action="store_true",
+                      help="debug mode")
+  parser.add_argument("-i", "--ignore", nargs='+', type=str,
+                      help="space separated list of files to be ignored",
+                      default=list())
+  args = parser.parse_args()
 
-for filePath in fileList:
-  if filePath in whitelist:
-    continue
-  with open(filePath, 'r', encoding='utf-8') as fileStream:
-    balance = [0 for x in range(7)]
-    mergeConflict = False
-    for lineNo, lineText in enumerate(fileStream):
-      balance[0] += lineText.count('"')
-      balance[1] += lineText.count('<')
-      balance[2] += lineText.count('>')
-      balance[3] += lineText.count('(')
-      balance[4] += lineText.count(')')
-      balance[5] += lineText.count('[')
-      balance[6] += lineText.count(']')
-      if re.match(r"[<>=]{7}", lineText):
-        mergeConflict = True
-      if re.match(r"^(\t| )*>", lineText):
-        balance[2] -= 1
-      for match in re.finditer(r"[a-zA-Z0-9](_[a-zA-Z0-9]+)+", lineText):
-        if re.search(r"<a id='[a-zA-Z0-9](_[a-zA-Z0-9]+?)+?'", lineText[match.start(0)-7:match.end(0)+6]):
-          tags.append([match.group(0), lineNo, match.start(0), filePath])
-        elif re.search(r"<a id=\"[a-zA-Z0-9](_[a-zA-Z0-9]+?)+?\"", lineText[match.start(0)-7:match.end(0)+6]):
-          tags.append([match.group(0), lineNo, match.start(0), filePath])
-        elif re.search(r"\[[^\[^\]]+?\]\(#[a-zA-Z0-9](_[a-zA-Z0-9]+)+\)", lineText[0:match.end(0)+1]):
-          refs.append([match.group(0), lineNo, match.start(0), filePath, False])
-        elif re.search(r'<a href="[^"]+?(_[a-zA-Z0-9]+)+">.+?</a>', lineText):
-          continue
-        elif re.search(r"\[[^\[^\]]+?\]\(http.+?\)", lineText[0:match.end(0)+1]):
-          continue
-        else:
-          write("Tag or reference malformed: " + match.group(0)
-            + "; line: " + str(lineNo+1)
-            + "; position: " + str(match.start(0))
-            + "; file: " + filePath
-            )
-    if balance[0]%2 != 0:
-      write("Unmatched \" in file: " + filePath)
-    if balance[1] != balance[2]:
-      write("Unmatched <> in file: " + filePath)
-    if balance[3] != balance[4]:
-      write("Unmatched () in file: " + filePath)
-    if balance[5] != balance[6]:
-      write("Unmatched [] in file: " + filePath)
-    if mergeConflict:
-      write("Merge conflict in file: " + filePath)
+  logger = logging.getLogger('journal_tester')
+  log_handler = logging.StreamHandler()
+  log_handler.setFormatter(
+      logging.Formatter(
+          '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+          )
+      )
+  logger.addHandler(log_handler)
+  if args.debug:
+      logger.setLevel(logging.DEBUG)
+  else:
+      logger.setLevel(logging.INFO)
 
-for n, x in enumerate(tags):
-  for o, y in enumerate(tags):
-    if o > n and x[0] == y[0]:
-      write("Duplicate tag found: " + y[0]
-        + "; first: line: " + str(x[1]+1)
-        + ", position: " + str(x[2])
-        + ", file: " + x[3]
-        + "; subsequent: line: " + str(y[1]+1)
-        + ", position: " + str(y[2])
-        + ", file: " + y[3]
+  whitelist = [
+    ".gitignore",
+    "requirements.txt",
+    ".travis.yml",
+    "test.py",
+    "tag.py"
+    ]
+  whitelist.extend(args.ignore)
+  with open(".gitignore") as gitignore:
+    whitelist.extend(gitignore.read().splitlines())
+  logger.debug("main: Ignored files: %s", str(whitelist))
+  files = [f for f in os.listdir() if os.path.isfile(f)]
+  files = list(set(files) - set(whitelist))
+  logger.debug("main: List of files: %s", str(files))
+
+  tags = []
+  refs = []
+
+  for filePath in files:
+    with open(filePath, 'r', encoding='utf-8') as fileStream:
+      balance = [0 for x in range(7)]
+      mergeConflict = False
+      for lineNo, lineText in enumerate(fileStream):
+        balance[0] += lineText.count('"')
+        balance[1] += lineText.count('<')
+        balance[2] += lineText.count('>')
+        balance[3] += lineText.count('(')
+        balance[4] += lineText.count(')')
+        balance[5] += lineText.count('[')
+        balance[6] += lineText.count(']')
+        if re.match(r"[<>=]{7}", lineText):
+          mergeConflict = True
+        if re.match(r"^(\t| )*>", lineText):
+          balance[2] -= 1
+        for match in re.finditer(r"[a-zA-Z0-9](_[a-zA-Z0-9]+)+", lineText):
+          if re.search(r"<a id='[a-zA-Z0-9](_[a-zA-Z0-9]+?)+?'", lineText[match.start(0)-7:match.end(0)+6]):
+            tags.append([match.group(0), lineNo, match.start(0), filePath])
+          elif re.search(r"<a id=\"[a-zA-Z0-9](_[a-zA-Z0-9]+?)+?\"", lineText[match.start(0)-7:match.end(0)+6]):
+            tags.append([match.group(0), lineNo, match.start(0), filePath])
+          elif re.search(r"\[[^\[^\]]+?\]\(#[a-zA-Z0-9](_[a-zA-Z0-9]+)+\)", lineText[0:match.end(0)+1]):
+            refs.append([match.group(0), lineNo, match.start(0), filePath, False])
+          elif re.search(r'<a href="[^"]+?(_[a-zA-Z0-9]+)+">.+?</a>', lineText):
+            continue
+          elif re.search(r"\[[^\[^\]]+?\]\(http.+?\)", lineText[0:match.end(0)+1]):
+            continue
+          else:
+            write("Tag or reference malformed: " + match.group(0)
+              + "; line: " + str(lineNo+1)
+              + "; position: " + str(match.start(0))
+              + "; file: " + filePath
+              )
+      if balance[0]%2 != 0:
+        write("Unmatched \" in file: " + filePath)
+      if balance[1] != balance[2]:
+        write("Unmatched <> in file: " + filePath)
+      if balance[3] != balance[4]:
+        write("Unmatched () in file: " + filePath)
+      if balance[5] != balance[6]:
+        write("Unmatched [] in file: " + filePath)
+      if mergeConflict:
+        write("Merge conflict in file: " + filePath)
+
+  for n, x in enumerate(tags):
+    for o, y in enumerate(tags):
+      if o > n and x[0] == y[0]:
+        write("Duplicate tag found: " + y[0]
+          + "; first: line: " + str(x[1]+1)
+          + ", position: " + str(x[2])
+          + ", file: " + x[3]
+          + "; subsequent: line: " + str(y[1]+1)
+          + ", position: " + str(y[2])
+          + ", file: " + y[3]
+          )
+
+  for ref in refs:
+    for tag in tags:
+      if ref[0] == tag[0]:
+        ref[4] = True
+    if not ref[4]:
+      write("Reference malformed: " + ref[0]
+        + "; line: " + str(ref[1]+1)
+        + "; position: " + str(ref[2])
+        + "; file: " + ref[3]
         )
 
-for ref in refs:
-  for tag in tags:
-    if ref[0] == tag[0]:
-      ref[4] = True
-  if not ref[4]:
-    write("Reference malformed: " + ref[0]
-      + "; line: " + str(ref[1]+1)
-      + "; position: " + str(ref[2])
-      + "; file: " + ref[3]
-      )
+  if 'CI' in os.environ:
+    if feedback != "":
+      r = requests.post('https://api.github.com/repos/'+os.environ['TRAVIS_REPO_SLUG']+'/commits/'+os.environ['TRAVIS_COMMIT']+'/comments',
+        json={"body": feedback},
+        auth=requests.auth.HTTPBasicAuth(os.environ['github_user'], os.environ['github_token'])
+        )
+    else:
+      r = requests.post('https://api.github.com/repos/'+os.environ['TRAVIS_REPO_SLUG']+'/commits/'+os.environ['TRAVIS_COMMIT']+'/comments',
+        json={"body": "Test passed!"},
+        auth=requests.auth.HTTPBasicAuth(os.environ['github_user'], os.environ['github_token'])
+        )
 
-if 'CI' in os.environ:
   if feedback != "":
-    r = requests.post('https://api.github.com/repos/'+os.environ['TRAVIS_REPO_SLUG']+'/commits/'+os.environ['TRAVIS_COMMIT']+'/comments',
-      json={"body": feedback},
-      auth=requests.auth.HTTPBasicAuth(os.environ['github_user'], os.environ['github_token'])
-      )
+    print(feedback)
+    sys.exit(1)
   else:
-    r = requests.post('https://api.github.com/repos/'+os.environ['TRAVIS_REPO_SLUG']+'/commits/'+os.environ['TRAVIS_COMMIT']+'/comments',
-      json={"body": "Test passed!"},
-      auth=requests.auth.HTTPBasicAuth(os.environ['github_user'], os.environ['github_token'])
-      )
+    print("Test passed!")
+    sys.exit(0)
 
-if feedback != "":
-  print(feedback)
-  sys.exit(1)
-else:
-  print("Test passed!")
-  sys.exit(0)
+if __name__ == "__main__":
+  main()
